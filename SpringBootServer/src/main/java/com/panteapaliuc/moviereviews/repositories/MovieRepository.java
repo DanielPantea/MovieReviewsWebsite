dd3a1ccc0c93616887afa0e4c964ad69a3cf55f6
danielpantea <pantea.daniel6@gmail.com>
2021-04-14 00:31:36 +0300

    Set up Spring Boot App

--- SpringBootServer/src/main/java/com/panteapaliuc/moviereviews/repositories/MovieRepository.java
+++
@@ -1,7 +0,0 @@
-package com.panteapaliuc.moviereviews.repositories;
-
-import com.panteapaliuc.moviereviews.models.Movie;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MovieRepository extends JpaRepository<Movie, Long> {
-}
