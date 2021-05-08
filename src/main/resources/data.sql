415618b589c4b0ffcbc562428cee228f0b5d14dd
Daniel <daniel.pantea@eurosysthem.ro>
2021-05-09 00:33:51 +0300

    Filled Some Movie Details

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,10 @@
 INSERT INTO TAG(TAG_NAME) VALUES
     ('Trending'), ('Action'), ('Comedy'), ('Romantic'), ('Adventure'), ('Musical'), ('Drama'), ('Animated'), ('Horror'), ('Sci-Fi'), ('War');
 
-INSERT INTO MOVIE(MOVIE_TITLE) VALUES
-    ('Avengers'),
-    ('Titanic'),
-    ('Harry Potter');
+INSERT INTO MOVIE(MOVIE_TITLE, POSTER_IMG_URL, MOVIE_DESC) VALUES
+    ('Avengers', 'assets/avengers_poster.jpg', 'A seventeen-year-old aristocrat falls in love with a kind but poor artist aboard the luxurious, ill-fated R.M.S. Titanic.'),
+    ('Titanic', 'assets/titanic_poster.jpg', 'Earth''s mightiest heroes must come together and learn to fight as a team if they are going to stop the mischievous Loki and his alien army from enslaving humanity.'),
+    ('Harry Potter', 'assets/harry_potter_poster.jpg', 'An orphaned boy enrolls in a school of wizardry, where he learns the truth about himself, his family and the terrible evil that haunts the magical world.');
 
 INSERT INTO MOVIES_TAGS (MOVIE_ID, TAG_ID) VALUES
     (1, 1),
